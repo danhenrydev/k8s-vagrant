4b7c1e4fb15c0b5b2f01b08fc36165be7c66809c
Daniel Henry <iamdanhenry@gmail.com>
2023-10-11 18:41:00 -0500

    Clean up worker provision script

--- provision/worker.sh
+++ provision/worker.sh
@@ -1,17 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "################################"
+# This script initializes each of the worker
+# nodes and joins them to the cluster.
+
+echo -e "\n################################"
 echo "#                              #"
-echo "#   Initializing Worker Node   #"
+echo "#    Creating Worker Node      #"
 echo "#                              #"
 echo "################################"
+echo -e "\nMachine: $(hostname -s)"
+set -euo pipefail
 
 
 # bash /vagrant/generated/join.sh
 
-
+echo -e "\n\n---------------------------------"
+echo "Joining $(hostname -s) to the Kubernetes cluster"
+echo -e "---------------------------------\n"
 eval "$(</vagrant/generated/join.sh) --node-name $(hostname -s)"
 
+echo -e "\n\n---------------------------------"
+echo "Preparing kubectl configuration"
+echo -e "---------------------------------\n"
 cp /vagrant/generated/config /etc/kubernetes/admin.conf
 
 sudo -u vagrant mkdir /home/vagrant/.kube
@@ -21,3 +31,9 @@ chown -R vagrant:vagrant /home/vagrant/.kube
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo -e "\n\n-------------------------------------------------"
+echo "Worker node $(hostname -s) configured, moving on "
+echo -e "-------------------------------------------------\n"
+
+kubectl get nodes
