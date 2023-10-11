0871ddfc6a0e850cd59fe7d73057bc4d68e4a50a
Daniel Henry <iamdanhenry@gmail.com>
2023-10-11 17:15:32 -0500

    Refactor init.sh

--- provision/init.sh
+++ provision/init.sh
@@ -1,21 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "################################"
+# This script initializes all of the provisioned machines
+# to make sure they are ready to be part of a kuberenetes
+# cluster.
+
+set -euo pipefail
+
+echo -e "\n################################"
 echo "#                              #"
 echo "#    Initial Configuration     #"
 echo "#                              #"
 echo "################################"
-
-set -euxo pipefail
 
 # Handle Arguments
 
 DNS_SERVERS=$1
 K8S_VERSION=$2
 
-echo "--- Setting DNS Configuration ---"
+echo -e "\n\n---------------------------------"
+echo "Setting DNS Configuration"
+echo -e "---------------------------------\n"
 
-# DNS Setting
+# MAke the resolved.conf.d dir if required
+# and add the DNS configuration into resolved
 if [ ! -d /etc/systemd/resolved.conf.d ]; then
 	sudo mkdir /etc/systemd/resolved.conf.d/
 fi
@@ -24,34 +31,50 @@ cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
 DNS=${DNS_SERVERS}
 EOF
 
+# Restart resolved to ensure DNS changes take effect
 sudo systemctl restart systemd-resolved
 
 
-echo "--- Disabling Swap ---"
+echo -e "\n\n---------------------------------"
+echo "Disabling Swap"
+echo -e "---------------------------------\n"
 
+# Turn off any existing swap, and create a crontab to do it on reboot
+# as well.
 sudo swapoff -a
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
 
 
-echo "--- Updating and Upgrading Packages ---"
+echo -e "\n\n---------------------------------"
+echo "Update and Upgrade Packages"
+echo -e "---------------------------------\n"
+
 sudo apt update -y
 sudo apt upgrade -y
 
-echo "--- Installing Required Packages ---"
+
+echo -e "\n\n---------------------------------"
+echo "Installing Required Packages"
+echo -e "---------------------------------\n"
+
 apt install -y ca-certificates curl gnupg lsb-release jq
 
 
+echo -e "\n\n---------------------------------------"
+echo "Configuring Kernel modules and Paramaters"
+echo -e "---------------------------------------\n"
 
-echo "--- Configuring Kernel Modules and Parameters ---"
-
-cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+# Add the overlay and br_netfilter modules on boot
+cat <<EOF | sudo tee /etc/modules-load.d/cri.conf
 overlay
 br_netfilter
 EOF
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+# enable the modules now
+modprobe overlay
+modprobe br_netfilter
 
+# Kuberentes sysctl requirements
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
@@ -61,22 +84,26 @@ EOF
 sudo sysctl --system
 
 
-echo "--- Installing Containerd  ---"
+echo -e "\n\n---------------------------------------"
+echo "Installing and Configuring containerd"
+echo -e "---------------------------------------\n"
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
-apt update -qq
-apt install -qq -y containerd.io
+apt update -qq  
+apt install -qq -y containerd.io  
 containerd config default > /etc/containerd/config.toml
 sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-systemctl restart containerd
+systemctl restart containerd 
 systemctl enable containerd
 
 
-echo "--- Installing Kubernetes Packages  ---"
+echo -e "\n\n---------------------------------"
+echo "Installing Kuberenetes Packages"
+echo -e "---------------------------------\n"
 
 sudo apt-get update -y
 if [ -z "$K8S_VERSION" ] || [[ "$K8S_VERSION" == "latest" ]]; then
