3ee66a831d512138db83a4badd0ac8d5908ac6ab
Daniel Henry <iamdanhenry@gmail.com>
2023-10-11 20:32:46 -0500

    Clean up metallb installation script

--- provision/metallb.sh
+++ provision/metallb.sh
@@ -1,15 +1,34 @@
-#!/bin/bash
-#
-# Deploys metallb to the cluster
+#!/usr/bin/env bash
 
+# This script installs metallb into the cluster
+
+echo -e "\n################################"
+echo "#                              #"
+echo "#    Installing metallb        #"
+echo "#                              #"
+echo "################################"
+echo -e "\nMachine: $(hostname -s)"
 set -euo pipefail
 
-printf "\nCreating metallb namespace\n"
+
+echo -e "\n\n---------------------------------"
+echo "Creating metallb-system namespace"
+echo -e "---------------------------------\n"
+
 sudo -i -u vagrant kubectl create namespace metallb-system
 
+
+echo -e "\n\n---------------------------------"
 echo "Applying metallb-native manifest from github"
+echo -e "---------------------------------\n"
+
 sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml >/dev/null 2>&1
-printf "\nManifest installed, installing metallb configuration\n\n"
+
+
+echo -e "\n\n---------------------------------"
+echo "Applying metallb configuration"
+echo -e "---------------------------------\n"
+
 # Maximum number of retries
 max_retries=10
 
@@ -19,7 +38,9 @@ retry_count=0
 while [ $retry_count -lt $max_retries ]; do
     # Run the command
     if sudo -i -u vagrant kubectl apply -f /vagrant/provision/yaml/metallb.yaml  >/dev/null 2>&1; then
-        printf "\nMETALLB INSTALLED\n\n"
+        echo -e "\n\n---------------------------------"
+        echo "Metallb installed, moving on"
+        echo -e "---------------------------------\n"
         break
     else
         # Command failed, increment retry count and wait before retrying
@@ -31,7 +52,9 @@ done
 
 # Check if the maximum number of retries was reached
 if [ $retry_count -eq $max_retries ]; then
-    echo "ERROR: METALLB INSTALLATION FAILED"
+    echo -e "\n\n##############################"
+    echo "METALLB INSTALLATION FAILED"
+    echo -e "##############################\n"
 fi
 
 
