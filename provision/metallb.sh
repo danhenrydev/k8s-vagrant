4e776353123c141c4c93503635868f724efc165c
Daniel Henry <iamdanhenry@gmail.com>
2023-10-16 09:28:06 -0500

    Increase default sleep time

--- provision/metallb.sh
+++ provision/metallb.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-#
-# Deploys metallb to the cluster
+#!/usr/bin/env bash
+
+# This script installs metallb into the cluster
 
 set -euo pipefail
 
@@ -8,7 +8,7 @@ printf "\nCreating metallb namespace\n"
 sudo -i -u vagrant kubectl create namespace metallb-system
 
 echo "Applying metallb-native manifest from github"
-sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml >/dev/null 2>&1
+sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.11/config/manifests/metallb-native.yaml
 printf "\nManifest installed, installing metallb configuration\n\n"
 # Maximum number of retries
 max_retries=10
@@ -19,16 +19,19 @@ retry_count=0
 cp /vagrant/provision/yaml/metallb.yaml /tmp/metallb.yaml
 sed -i "s/<<ADDRESS_RANGE>>/$1/g" /tmp/metallb.yaml
 
+sleep 30
+
 while [ $retry_count -lt $max_retries ]; do
     # Run the command
-    if sudo -i -u vagrant kubectl apply -f /tmp/metallb.yaml  >/dev/null 2>&1; then
+    if sudo -i -u vagrant kubectl apply -f /tmp/metallb.yaml 
+    then
         printf "\nMETALLB INSTALLED\n\n"
         break
     else
         # Command failed, increment retry count and wait before retrying
         retry_count=$((retry_count + 1))
         echo "Waiting for metallb to come online..."
-        sleep 15  # Adjust the sleep duration as needed
+        sleep 30  # Adjust the sleep duration as needed
     fi
 done
 
